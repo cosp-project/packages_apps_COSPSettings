c88d0ce805f6cd8811081dd61136651c9dcb185e
edi194 <misiakadrian@gmail.com>
2019-01-22 12:40:05 +0100

    cospsettings: navbar toggle

--- src/com/cosp/settings/fragments/NavbarSettings.java
+++ src/com/cosp/settings/fragments/NavbarSettings.java
@@ -39,6 +39,11 @@ import com.cosp.settings.R;
 
 public class NavbarSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+			
+			
+		private SwitchPreference mNavbarToggle;
+		
+		
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,15 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.cosp_settings_navbar);
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+		
+		mNavbarToggle = (SwitchPreference) findPreference("navigation_bar_enabled");
+        boolean enabled = Settings.Secure.getIntForUser(
+                resolver, Settings.Secure.NAVIGATION_BAR_ENABLED,
+                getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar) ? 1 : 0,
+                UserHandle.USER_CURRENT) == 1;
+        mNavbarToggle.setChecked(enabled);
+        mNavbarToggle.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -63,7 +77,16 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
         super.onPause();
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {	
+			        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mNavbarToggle) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    Settings.Secure.NAVIGATION_BAR_ENABLED, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mNavbarToggle.setChecked(value);
+            return true;
+        }
         return false;
     }
 }
